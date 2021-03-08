93a526c1240e0fc1b97eb82302019b33043383cb
patryks9090@gmail.com <Patryk0.1>
2021-03-08 08:59:52 +0100

    navbar added

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let nav = '<nav> <ul>\n' +
+    '    <li><a href="index.html">Home</a></li>\n' +
+    '    <li><a href="contact.html">Kontakt</a></li>\n' +
+    '    <li><a href="newPage.html">Test</a></li>\n' +
+    '</ul></nav> ';
+
+let elem = document.querySelector(".navbar");
+
+elem.innerHTML = nav;
--- zad2scipt.js
+++ zad2scipt.js
@@ -1,5 +1,5 @@
 for(let i = 1; i < 10; i++){
-    var array = [];
+    let array = [];
     for(let j = 1; j < 10; j++) {
         array.push(i*j);
     }
